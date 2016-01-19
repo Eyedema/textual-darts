13c2f22cfcc0adb71e7c80edc7e61a14b246bbc6
Ubaldo <u.puocci@gmail.com>
2016-01-19 14:25:31 +0100

    modified sorting algorithm to match jre 1.7

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -15,9 +16,8 @@ public class Game extends GameTurn {
 			gioca(giocatori.get(turno));
 			passaTurno();
 			summarize();
-		} 
+		}
 	}
-	
 
 	private void inserisciPunti() {
 		Scanner scanner = new Scanner(System.in);
@@ -29,7 +29,7 @@ public class Game extends GameTurn {
 			}
 		}
 		Iterator<Player> it = giocatori.iterator();
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			Player current = it.next();
 			current.setPoints(puntiTotali);
 		}
@@ -39,16 +39,26 @@ public class Game extends GameTurn {
 		System.out.println("\n\n");
 		System.out.println("\n%*%*%*%*%*%*%*%*%*%*%*%*%*%*%*%*");
 		System.out.println("Situazione attuale:\n");
-	    Comparator<Player> sorted = (e1, e2) -> Integer.compare(
-	            e1.getPoints(), e2.getPoints());
+		// Comparator<Player> sorted = (e1, e2) -> Integer.compare(
+		// e1.getPoints(), e2.getPoints());
+		// giocatori.stream().sorted(sorted)
+		// .forEach(e -> e.getInfo());
 
-	    giocatori.stream().sorted(sorted)
-	            .forEach(e -> e.getInfo());
+		// Sorting
+		Collections.sort(giocatori, new Comparator<Player>() {
+			@Override
+			public int compare(Player p1, Player p2) {
+
+				return Integer.compare(p1.getPoints(), p2.getPoints());
+			}
+		});
+		for (int i = 0; i < giocatori.size(); i++) {
+			giocatori.get(i).getInfo();
+		}
+
 		System.out.println("%*%*%*%*%*%*%*%*%*%*%*%*%*%*%*%*");
 		System.out.println("\n\n");
 	}
-
-	
 
 	public void Welcome() {
 		System.out.println("\n\n");
