a3f255bfc076eac26e6482c68eeca892205bebab
Ubaldo <u.puocci@gmail.com>
2016-01-20 19:05:02 +0100

    Added options with arguments

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Game g = new Game();
+		List<String> s = Arrays.asList(args);
+		if (s.isEmpty()) {
+			Game g = new Game();
+		} else {
+			FriendsGame f = new FriendsGame(s);
+		}
 	}
 }
